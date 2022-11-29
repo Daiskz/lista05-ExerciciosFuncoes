3d3fc7313039b59ff744a9db2a89edc7a9b17db6
Daiskz <gabriel.matsubara@gmail.com>
2022-11-29 16:44:17 -0300

    Resolução Exercício 5

---
+++ ListaExerciciosFuncoes/src/Exercicio5/Main.java
@@ -0,0 +1,7 @@
+package Exercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+        SomaImposto.somaImposto(5, 200);
+    }
+}
---
+++ ListaExerciciosFuncoes/src/Exercicio5/SomaImposto.java
@@ -0,0 +1,8 @@
+package Exercicio5;
+
+public class SomaImposto {
+    public static void somaImposto(double taxaImposto, double custo) {
+        System.out.println((0.01 * taxaImposto) * custo + custo);
+    }
+    
+}
