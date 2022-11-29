f62b1b8939cfc72d437840ff4f62819a252255d7
Daiskz <gabriel.matsubara@gmail.com>
2022-11-29 18:14:14 -0300

    Resolução Exercício 9

---
+++ ListaExerciciosFuncoes/src/Exercicio9/ReversoNumero.java
@@ -0,0 +1,16 @@
+package Exercicio9;
+
+import java.util.Scanner;
+
+public class ReversoNumero {
+    public static void numeroReverso() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        String numeroReverso = entrada.next();
+        for (int i = numeroReverso.length(); i > 0; i--) {
+            System.out.print(numeroReverso.charAt(i-1));
+        }
+        System.out.println("");
+        entrada.close();
+    }
+}
