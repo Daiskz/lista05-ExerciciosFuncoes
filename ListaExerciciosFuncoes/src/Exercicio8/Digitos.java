b11fad0aa636eba9304b27bba059daf8a60d9cdb
Daiskz <gabriel.matsubara@gmail.com>
2022-11-29 18:01:52 -0300

    Resolução Exercício 8

---
+++ ListaExerciciosFuncoes/src/Exercicio8/Digitos.java
@@ -0,0 +1,14 @@
+package Exercicio8;
+
+import java.util.Scanner;
+
+public class Digitos {
+    public static void quantidadeDigitos() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número inteiro");
+        String numero = entrada.next();
+        int numeroConvertido = numero.length();
+        System.out.println("A quantidade de digitos no número é: " +numeroConvertido);
+        entrada.close();
+    }
+}
---
+++ ListaExerciciosFuncoes/src/Exercicio8/README.md
@@ -0,0 +1,7 @@
+## Projeto Modelo para Atividade Java X GitHub
+
+Sejam bem-vindos ao projeto.
+
+## Enunciado
+
+Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
