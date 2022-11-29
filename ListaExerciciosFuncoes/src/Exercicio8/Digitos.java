b9af189e395273b4724732ec04be68f16059fd7e
Daiskz <gabriel.matsubara@gmail.com>
2022-11-29 18:15:02 -0300

    Resolução Exercício 8

--- ListaExerciciosFuncoes/src/Exercicio8/Digitos.java
+++ ListaExerciciosFuncoes/src/Exercicio8/Digitos.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Digitos {
     public static void quantidadeDigitos() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite um número inteiro");
+        System.out.println("Digite um número inteiro: ");
         String numero = entrada.next();
         int numeroConvertido = numero.length();
         System.out.println("A quantidade de digitos no número é: " +numeroConvertido);
