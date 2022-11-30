c4bc6d2cbb1f3928007418115f77b98d62b9afb4
Daiskz <gabriel.matsubara@gmail.com>
2022-11-30 13:45:09 -0300

    Resolução Exercício 4

--- ListaExerciciosFuncoes/src/Exercicio_4/Main.java
+++ ListaExerciciosFuncoes/src/Exercicio_4/Main.java
@@ -1,4 +1,4 @@
-package Exercicio4;
+package Exercicio_4;
 
 public class Main {
     public static void main(String[] args) {
--- ListaExerciciosFuncoes/src/Exercicio_4/SinalNumero.java
+++ ListaExerciciosFuncoes/src/Exercicio_4/SinalNumero.java
@@ -1,4 +1,4 @@
-package Exercicio4;
+package Exercicio_4;
 
 import java.util.Scanner;
 
